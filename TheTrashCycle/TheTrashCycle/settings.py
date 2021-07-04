8d304d8e5cc3cd3c0290ef5a9e58bb717954b418
Akumar144 <akumar144@student.fuhsd.org>
2021-07-04 11:40:56 -0700

    Updated django

--- TheTrashCycle/TheTrashCycle/settings.py
+++ TheTrashCycle/TheTrashCycle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm8m865=+!f$0mjmnpuq&f9v9b@0bl6)&p#j%o!6b)6k$mecz1p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thetrashcycle.ml', "thelifecycle.ml", "localhost", "192.168.1.3", "127.0.0.1", "www.thelifecycle.ml", "192.168.1.4", "73.71.159.154"]
+ALLOWED_HOSTS = ['thetrashcycle.ml', "thelifecycle.ml", "localhost", "192.168.1.3", "127.0.0.1", "www.thelifecycle.ml", "192.168.1.4", "73.71.159.154", "*"]
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
