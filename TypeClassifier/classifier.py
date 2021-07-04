944097385fdaa8e3413cf7c7683ba871fed3effb
Aman <71572787+QuackingBob@users.noreply.github.com>
2021-07-04 12:08:52 -0700

    Update classifier.py

--- TypeClassifier/classifier.py
+++ TypeClassifier/classifier.py
@@ -6,8 +6,8 @@ import cv2
 model = tf.keras.models.load_model("type-classifier3")
 
 def get_pred(image):
-    # im_arr = np.array(image)
-    im_arr = cv2.resize(image, (150,150))
+    im_arr = np.array(image)
+    im_arr = cv2.resize(im_arr, (150,150))
     im_arr = np.array([im_arr])
     raw_pred = model.predict(im_arr)[0]
     pred = np.argmax(raw_pred)
@@ -15,9 +15,3 @@ def get_pred(image):
 
 def get_class(pred):
     return "organic or non-recyclable" if pred == 0 else "recyclable"
-
-im_arr = plt.imread(r"D:\OneDrive\ByteKode Hackathon\TrashClassifier\Dataset 2\archive\DATASET\TEST\O\O_12631.jpg")
-plt.plot()
-plt.imshow(im_arr)
-pred = get_pred(im_arr)
-print(get_class(pred))
