149b573b6dde8c69c6a15f42c3b214ebd3660e5f
Akumar144 <akumar144@student.fuhsd.org>
2021-07-03 17:51:25 -0700

    Updated django

--- TheTrashCycle/TheTrashCycle/settings.py
+++ TheTrashCycle/TheTrashCycle/settings.py
@@ -127,5 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = "main/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
