152f822f73c3e694b3d2ca7cd368cb617b7be4ef
Akumar144 <akumar144@student.fuhsd.org>
2021-07-04 20:39:04 -0700

    Updated django

--- TheTrashCycle/main/apps.py
+++ TheTrashCycle/main/apps.py
@@ -1,7 +1,2 @@
 from django.apps import AppConfig
 import os
-import tensorflow as tf
-
-class MainConfig(AppConfig):
-    name = 'main'
-    type_model = tf.keras.models.load_model('main/models/type-classifier3')
